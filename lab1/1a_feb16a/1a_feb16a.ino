a76503097011e4b187ae715f75b42333ba404869
STomlinson1 <53631071+STomlinson1@users.noreply.github.com>
2021-02-16 15:32:03 -0500

    first commit lab 1a

---
+++ lab1/1a_feb16a/1a_feb16a.ino
@@ -0,0 +1,20 @@
+#include <Pololu3piPlus32U4.h>
+
+using namespace Pololu3piPlus32U4;
+
+Encoders encoders;
+LCD lcd;
+Buzzer buzzer;
+Motors motors;
+ButtonA buttonA;
+ButtonC buttonC;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
