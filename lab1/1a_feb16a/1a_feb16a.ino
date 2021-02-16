a4ee3e6cf4d4890f236b19ed9080a0d88cda3a98
STomlinson1 <53631071+STomlinson1@users.noreply.github.com>
2021-02-16 18:48:39 -0500

    lab 1a finish

--- lab1/1a_feb16a/1a_feb16a.ino
+++ lab1/1a_feb16a/1a_feb16a.ino
@@ -8,20 +8,66 @@ Buzzer buzzer;
 Motors motors;
 ButtonA buttonA;
 
+unsigned long currentMillis;
+unsigned long prevMillis;
+const unsigned long PERIOD = 15;
+
+long countsLeft = 0;
+long countsRight = 0;
+long prevLeft = 0;
+long prevRight = 0;
+
+const int CLICKS_PER_ROTATION = 12;
+const float GEAR_RATIO = 75.81F;
+const float WHEEL_DIAMETER = 3.2;
+const float WHEEL_CIRCUMFERENCE = 10.0531F;
+
+//distance traveled by left/right wheels
+float Sl = 0.0F;
+float Sr = 0.0F;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(57600);
   delay(1000);
+  // play a beep sound 
   buzzer.play("c32");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(buttonA.isPressed()){
-    motors.setSpeeds(100, 100);
-    Serial.println("Motors Running!");
-  }
-  else{
-    motors.setSpeeds(0,0);
+  checkEncoders();
+}
+
+void checkEncoders(){
+  currentMillis = millis();
+  if(currentMillis > prevMillis + PERIOD){
+    countsLeft += encoders.getCountsAndResetLeft();
+    countsRight += encoders.getCountsAndResetRight();
+
+    // Distance achieved in cm
+    Sl += ((countsLeft - prevLeft) / (CLICKS_PER_ROTATION * GEAR_RATIO) * WHEEL_CIRCUMFERENCE);
+    Sr += ((countsRight - prevRight) / (CLICKS_PER_ROTATION * GEAR_RATIO) * WHEEL_CIRCUMFERENCE);
+
+    int wheelSpeed = 80;
+    if(Sr < 30.3){
+      // Add a slow down after 20cm
+      if(Sr > 20){
+        wheelSpeed = 75 * ((30 - Sr)/ 10);
+        if(wheelSpeed < 25) wheelSpeed = 25;
+      }
+      motors.setSpeeds(wheelSpeed, wheelSpeed);
+    }else{
+      motors.setSpeeds(0,0);
+    }
+    
+    Serial.print("Left: ");
+    Serial.print(Sl);
+    Serial.print(" Right: ");
+    Serial.println(Sr);
+
+    prevLeft = countsLeft;
+    prevRight = countsRight;
+    prevMillis = currentMillis;
   }
 }
